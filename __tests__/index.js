7738d7c321bb4bb33642f77301c33a3d14ee9c86
twinh <twinhuang@qq.com>
2021-05-09 22:15:45 +0800

    test: 移除 `core-js` 和 `regenerator-runtime` 依赖

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,6 +1,3 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
 import React from 'react';
 import {render} from '@testing-library/react';
 import {Form, FormItem} from '@mxjs/a-form';
